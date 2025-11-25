40117e39cd4bea262b60a7bbc17bc5c53ab427e6
たね <78518345+tane2222@users.noreply.github.com>
2025-11-25 23:02:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -449,7 +449,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 
                 // 画像が未設定、または placehold.jp などのデフォルト画像の場合に表示
                 // ※正規表現でチェック
-                const defaultImgPattern = 'https://drive.google.com/thumbnail?id=12DqJms_8Fr8BTYzCaGlFFW82Nmf3B4Q0'; 
+                const defaultImgPattern = 'https://drive.google.com/thumbnail?id=19TYKEgpS-EKBzyioGfqM7xZJRjQb67tk'; 
                 
                 if (!currentImgUrl || defaultImgPattern.test(currentImgUrl)) {
                      promoSection.style.display = 'block';
