0bb5f4aa44033bdf868a8d2f0cc7d59f13cf3c83
たね <78518345+tane2222@users.noreply.github.com>
2025-12-02 17:13:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -743,9 +743,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
     async function loadNewUserListPage() {
         const swipeDeck = document.getElementById('swipe-deck');
-          swipeDeck.innerHTML = '<p>ユーザーを探しています...</p>';
+        //swipeDeck.innerHTML = '<p>ユーザーを探しています...</p>';
         // ▼ 修正: 文字を出さずに空にする（これで背景のロゴが見えます） ▼
         //swipeDeck.innerHTML = '';
+        
 
         try {
             const result = await callGasApi('getUsersForLiff', { liffUserId: liff.getContext().userId });
