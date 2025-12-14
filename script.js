db60e6fdbfb88a38a5fd219b66041b620a22008d
たね <78518345+tane2222@users.noreply.github.com>
2025-12-15 01:04:59 +0900

    Modify GAS_API_URL comment for clarity
    Updated GAS_API_URL comment to indicate user modification.

--- script.js
+++ script.js
@@ -1139,9 +1139,10 @@ var vueApp = new Vue({
         })
       };
 
-// ★★★★★★
-var GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwyKAZqLjwcc_Z_8ZLinHOhaGFcUPd9n_Asjf52oYbVpX3Kj3XYTT5cTiyO3luxiHGL3Q/exec';
-       fetch(GAS_API_URL,  
+// ★★★注意：以下のURLはあなたのGASデプロイURLに書き換えてください★★★
+     //  var GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwyKAZqLjwcc_Z_8ZLinHOhaGFcUPd9n_Asjf52oYbVpX3Kj3XYTT5cTiyO3luxiHGL3Q/exec'; 
+        
+       fetch(GAS_API_URL,  {
         method: 'POST',
         body: JSON.stringify(payload)
       })
